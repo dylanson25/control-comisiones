1919c09d8c77056b58de04e5b2c65f9fb80cdfb1
Dylanson25 <dvillarreal0@ucol.mx>
2024-02-10 09:07:25 -0600

    Instalacion oruga con bootstrap

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,15 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import Oruga from '@oruga-ui/oruga-next'
+import { bootstrapConfig } from '@oruga-ui/theme-bootstrap'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(Oruga, bootstrapConfig)
 app.use(createPinia())
 app.use(router)
 

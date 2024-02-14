a4b41a4173fd2ede930f2263641df276ded445da
Dylanson25 <dvillarreal0@ucol.mx>
2024-02-14 11:52:31 -0600

    add prefix to enviroment constants

--- env.d.ts
+++ env.d.ts
@@ -2,13 +2,13 @@
 
 interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string
-  readonly API_KEY_FIREBASE: string
-  readonly AUTH_DOMAIN_FIREBASE: string
-  readonly PROJECT_ID_FIREBASE: string
-  readonly STORAGE_BUCKET_FIREBASE: string
-  readonly MESSAGING_SENDER_ID_FIREBASE: string
-  readonly APP_ID_FIREBASE: string
-  readonly MEASUREMENT_ID_FIREBASE: string
+  readonly VITE_APP_API_KEY_FIREBASE: string
+  readonly VITE_APP_AUTH_DOMAIN_FIREBASE: string
+  readonly VITE_APP_PROJECT_ID_FIREBASE: string
+  readonly VITE_APP_STORAGE_BUCKET_FIREBASE: string
+  readonly VITE_APP_MESSAGING_SENDER_ID_FIREBASE: string
+  readonly VITE_APP_ID_FIREBASE: string
+  readonly VITE_APP_MEASUREMENT_ID_FIREBASE: string
 }
 
 interface ImportMeta {

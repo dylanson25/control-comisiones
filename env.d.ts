dca73e07bfd32f07a45a7e7b3e19d558cad6ddd8
Dylanson25 <dvillarreal0@ucol.mx>
2024-02-14 11:29:02 -0600

    Variables de entorno

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+  readonly API_KEY_FIREBASE: string
+  readonly AUTH_DOMAIN_FIREBASE: string
+  readonly PROJECT_ID_FIREBASE: string
+  readonly STORAGE_BUCKET_FIREBASE: string
+  readonly MESSAGING_SENDER_ID_FIREBASE: string
+  readonly APP_ID_FIREBASE: string
+  readonly MEASUREMENT_ID_FIREBASE: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}

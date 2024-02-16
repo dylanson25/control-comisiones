14a17b66661ca167898acba37a744699cfc67204
Dylanson25 <dvillarreal0@ucol.mx>
2024-02-15 19:16:46 -0600

    auth store

---
+++ src/stores/auth.ts
@@ -0,0 +1,34 @@
+/* eslint-disable no-unused-labels */
+import { sigInpWithEmail, getUserData, signOutWithEmail } from '@/utils/firebase/firebaseAuth'
+import { defineStore } from 'pinia'
+
+interface State {
+  userData: UserData
+}
+
+interface UserData {
+  uid?: string
+  displayName?: string
+  email?: string
+  userType?: string
+  userTypeId?: string
+}
+export const useAuthStore = defineStore('auth', {
+  state: (): State => ({
+    userData: {}
+  }),
+  actions: {
+    async onLogIn(email: string, pasword: string) {
+      const data = await sigInpWithEmail(email, pasword)
+      this.userData = data
+    },
+    async getUserData() {
+      const data = await getUserData()
+      this.userData = data
+    },
+    async onSignOut() {
+      await signOutWithEmail()
+      this.userData = {}
+    }
+  }
+})

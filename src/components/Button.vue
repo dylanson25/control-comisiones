98f303ae15268bc85a34854a702d9c717afb09d3
Dylanson25 <dvillarreal0@ucol.mx>
2024-02-16 18:56:47 -0600

    button loading prop

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import type { buttonProps } from './props'
 const props = defineProps<buttonProps>()
 const emit = defineEmits<{
   (e: 'click', value: PointerEvent): void
 }>()
+const isDisabled = computed(() => props.disabled || props.loading)
 </script>
 <template>
-  <o-button v-bind="{ ...props }" @click="emit('click', $event)">{{ props.label }}</o-button>
+  <o-button v-bind="{ ...props }" :disabled="isDisabled" @click="emit('click', $event)">
+    <o-icon v-if="props.loading" icon="spinner" spin />
+    <template v-else>
+      {{ props.label }}
+    </template>
+  </o-button>
 </template>

c61a872624fce2c47327e84e34e296d34f633512
Dylanson25 <dvillarreal0@ucol.mx>
2024-02-21 10:28:56 -0600

    initial product card component

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Badge } from '@/components'
+</script>
+
+<template>
+  <article class="card">
+    <div class="card-body">
+      <!-- TODO: Poner la tag de pk -->
+      <Badge label="PK15" size="6" variant="secondary" />
+      <hr />
+      <p class="mb-1"><span class="fw-bolder">Hotcakes:</span> <small>15 pz</small></p>
+      <p class="mb-1"><span class="fw-bolder">Precio:</span> <small>$15.00</small></p>
+      <p class="mb-1"><span class="fw-bolder">toppings incluidos:</span> <small>2</small></p>
+    </div>
+    <div class="card-footer">
+      <small class="text-body-secondary">last updated 3 mins ago</small>
+    </div>
+  </article>
+</template>
+
+<style lang="sass" scoped></style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as Field } from './Field.vue'
 export { default as Button } from './Button.vue'
 export { default as Badge } from './Badge.vue'
+
+export { default as ProductCard } from './ProductCard.vue'

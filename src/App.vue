f1869c2affbaecc513627627e686ec7b46e71d22
dylanson25 <dvillarreal0@ucol.mx>
2024-04-10 19:43:59 -0700

    remove aside menu homeView

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,15 @@
 <script setup lang="ts">
-import { RouterView, useRouter, useRoute } from 'vue-router'
+import { RouterView, useRouter } from 'vue-router'
 import { useAuthStore } from '@/stores'
-import { DefaultLayout } from '@/layouts'
-import { computed } from 'vue'
 
-const route = useRoute()
 const authStore = useAuthStore()
 const { push } = useRouter()
 
-const needAsideMenu = computed<boolean>(() => {
-  return Boolean(route.meta.asideMenu)
-})
-
 authStore.$subscribe((mutation, state) => {
-  if (state.userData.uid) push({ name: 'home' })
+  if (state.auth === 'authenticated') push({ name: 'home' })
 })
 </script>
 
 <template>
-  <DefaultLayout v-if="needAsideMenu">
-    <RouterView />
-  </DefaultLayout>
-  <RouterView v-else />
+  <RouterView />
 </template>
